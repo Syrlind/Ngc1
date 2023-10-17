c9536a5aa1cf87dc0bafa468fd14345ec9266e4a
stanislaus <stanislaus_johan@yahoo.com>
2023-10-17 16:15:24 +0700

    third commit

---
+++ Task2.py
@@ -0,0 +1,15 @@
+"""Task 2 , given data and do something about it """
+Data = [1, 4, 9, 16, 25, 36, 49,  64, 81, 100]
+
+"""TAsk 2.a"""
+
+print(Data[3]) 
+
+"""Task 2.b """
+
+print(Data[5:9:1])
+
+"""Task2.c"""
+
+print(Data[10:0:-1])
+
---
+++ Task3.py
@@ -0,0 +1,13 @@
+"""Task3"""
+
+provinsi = {'Nanggroe Aceh Darussalam': 'Aceh','Sumatera Selatan': 'Palembang'
+            ,'Kalimantan Barat': 'Pontianak','Jawa Timur': 'Madiun'
+            ,'Sulawesi Selatan': 'Makassar','Maluku': 'Ambon'}
+
+"""Task 3a"""
+print(provinsi.keys())
+
+"print(provinsi['Jawa Timur'])"
+"""Task3b"""
+provinsi.update({'Jawa Timur':'Surabaya'})
+print(provinsi)
